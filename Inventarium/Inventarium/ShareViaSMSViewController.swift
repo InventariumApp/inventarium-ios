486bd0c158875f7d8b027859c23f8f559dffac0d
Michael Rosenfield <mer95@case.edu>
2017-04-02 22:02:11 -0400

    added cancel button

---
+++ Inventarium/Inventarium/ShareViaSMSViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ShareViaSMSViewController.swift
+//  Inventarium
+//
+//  Created by Michael Rosenfield on 4/2/17.
+//  Copyright © 2017 Inventarium. All rights reserved.
+//
+
+import UIKit
+
+class ShareViaSMSViewController: UIViewController {
+
+    @IBAction func cancelClicked(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
