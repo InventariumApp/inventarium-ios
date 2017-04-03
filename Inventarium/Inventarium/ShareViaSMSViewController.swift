e72428a82b706e82390f6977dcfc32763d67a96a
Michael Rosenfield <mer95@case.edu>
2017-04-02 22:27:18 -0400

    Keyboard now hides on phone input

--- Inventarium/Inventarium/ShareViaSMSViewController.swift
+++ Inventarium/Inventarium/ShareViaSMSViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class ShareViaSMSViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     @IBAction func cancelClicked(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
 
     }
+    
+    @IBAction func shareClicked(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(ShareViaSMSViewController.keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil);
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ShareViaSMSViewController.keyboardWillHide(notification:)), name:NSNotification.Name.UIKeyboardWillHide, object: nil);
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tap(gesture:)))
+        self.view.addGestureRecognizer(tapGesture)
     }
+    
+    func tap(gesture: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func keyboardWillShow(_ notification: NSNotification) {
+        let keyboardHeight = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as AnyObject).cgRectValue.height
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.view.window?.frame.origin.y = -1 * keyboardHeight
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.view.window?.frame.origin.y = 0
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
